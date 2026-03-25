1924d53d81b4cc65155a677769f2615284312d04
eedkbr <klaus.brockmans@gmail.com>
2026-03-25 12:45:09 +0100

    stand-alone version for smartphone

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', function(event) {
-  // Erlaubt das Laden der App
+const CACHE_NAME = 'vokabel-trainer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './lists/vokabeln.csv' // Hier fügen wir deine Liste hinzu!
+];
+
+// 1. Installation: Dateien in den Cache laden
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 2. Abfangen: Wenn kein Netz da ist, aus dem Cache laden
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
